ef1abd879b9881906a41c1118e961150485d71b0
Коваленко Роман Александрович <r.kovalenko@bftcom.com>
2023-11-16 23:10:27 +0300

    Update my program Example_003_sum

--- Example_003_sum/Program.cs
+++ Example_003_sum/Program.cs
@@ -1,3 +1,5 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(1,10);
+int numberB = new Random().Next(1,10);
+
+Console.WriteLine(numberA + " and " + numberB);
 Console.Write(numberA+numberB);
