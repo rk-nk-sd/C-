1d500b3cbb59bf7b32f4bc2ddab26c32e24c43d7
Коваленко Роман Александрович <r.kovalenko@bftcom.com>
2023-11-16 23:37:47 +0300

    Add new project Example_005_ifElse

---
+++ Example_005_ifElse/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "Маша") {
+    System.Console.WriteLine("Машку за ляшку! Кого я вижу!");
+} else {
+    System.Console.WriteLine("Привет, " + username);
+}
