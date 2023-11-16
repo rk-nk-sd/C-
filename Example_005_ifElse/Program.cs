806de1af8e293db169c077189c5d837e6e54f8a7
Коваленко Роман Александрович <r.kovalenko@bftcom.com>
2023-11-16 23:43:43 +0300

    don't work function ToLower() - adding comment to code

--- Example_005_ifElse/Program.cs
+++ Example_005_ifElse/Program.cs
@@ -2,6 +2,7 @@
 Console.WriteLine("Введите имя пользователя: ");
 string username = Console.ReadLine();
 
+// username.ToLower() - эта шляпа не работает, наверное из библиотеки тянется
 if (username.ToLower() == "Маша") {
     System.Console.WriteLine("Машку за ляшку! Кого я вижу!");
 } else {
